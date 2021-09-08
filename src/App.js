93801de3ba350b800a9a8d837788798cc9321eaa
Ivonaldo Soares <vonaldojrsoares@gmail.com>
2021-09-08 20:39:13 -0300

    adiciona rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.scss';
 import Header from './components/shared/Header/Header';
 import Footer from './components/shared/Footer/Footer';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import MusicAdd from './pages/MusicAdd/MusicAdd';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <Switch>
+          <Route path="/" exact={true} component={Home}/>
+          <Route path="/add" component={MusicAdd}/>
+        </Switch>
       <Footer/>
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Pagina da home/Litagem da musica
+      Pagina da home
     </div>
   )
 }
